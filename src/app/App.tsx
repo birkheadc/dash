eb030244d92dd0d28a20ba84a86ac6fe0a81e532
Colby Birkhead <birkheadc@gmail.com>
2024-11-16 18:13:25 +0900

    added dummy text

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,6 +8,7 @@ function App() {
   return (
     <ContextProviders>
       <BrowserRouter>
+        Colby's Dashboard
         <PageContainer></PageContainer>
       </BrowserRouter>
     </ContextProviders>
